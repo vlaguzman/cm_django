843bd4fc5d517792b7976925c3e9dd700c02c58a
Vladimir Guzman <guzman.vla@gmail.com>
2013-01-16 17:58:51 -0500

    menu de navegacion agregado

---
+++ principal/admin.py
@@ -0,0 +1,10 @@
+from principal.models import Idea, Comentario, Tarea, TareaxIdea, Aplicacion, Perfil
+from django.contrib import admin
+
+admin.site.register(Idea)
+admin.site.register(Comentario)
+admin.site.register(Tarea)
+admin.site.register(TareaxIdea)
+admin.site.register(Aplicacion)
+admin.site.register(Perfil)
+
---
+++ principal/forms.py
@@ -0,0 +1,32 @@
+#encoding:utf-8
+from django.forms import ModelForm
+from django import forms
+from principal.models import Idea, Comentario, Tarea, TareaxIdea, Aplicacion, Calificacion
+
+class ContactoForm(forms.Form):
+	correo = forms.EmailField(label='Tu correo electrónico')
+	mensaje = forms.CharField(widget=forms.Textarea)
+
+class IdeaForm(ModelForm):
+	class Meta:
+		model = Idea
+
+class ComentarioForm(ModelForm):
+	class Meta:
+		model = Comentario
+			
+class TareaForm(ModelForm):
+	class Meta:
+		model = Tarea
+			
+class TareaIdeaForm(ModelForm):
+	class Meta:
+		model = TareaxIdea								
+
+class AplicacionForm(ModelForm):
+	class Meta:
+		model = Aplicacion								
+
+class CalificacionForm(ModelForm):
+	class Meta:
+		model = Calificacion
