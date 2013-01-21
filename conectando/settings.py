a4b4a12632dd1498f30806e9b6a4a1a6364fb065
Vladimir Guzman <guzman.vla@gmail.com>
2013-01-20 20:06:03 -0500

    corrigiendo error de carga de archivos estaticos

--- conectando/settings.py
+++ conectando/settings.py
@@ -76,9 +76,9 @@ MEDIA_ROOT = os.path.join(RUTA_PROYECTO, 'carga')
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/" exp. STATIC_ROOT = ''
+# Example: "/home/media/media.lawrence.com/static/" exp. STATIC_ROOT = 'os.path.join(RUTA_PROYECTO,'static')'
 
-STATIC_ROOT= os.path.join(RUTA_PROYECTO,'static')
+STATIC_ROOT= ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
